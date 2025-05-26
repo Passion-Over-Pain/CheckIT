b8ee8e770a3a709bac56a4532505f4a4129509d0
Tinotenda Mhedziso <tinomhedziso22@gmail.com>
2025-05-26 08:46:19 +0200

    An IDE Error occurred?
    1 Just check here future me :)

---
+++ app/src/main/java/com/example/learningmanagementsystem/Student.java
@@ -0,0 +1,4 @@
+package com.example.learningmanagementsystem;
+
+public class Student {
+}
--- app/src/main/java/com/example/learningmanagementsystem/ViewStudentList.java
+++ app/src/main/java/com/example/learningmanagementsystem/ViewStudentList.java
@@ -1,6 +1,11 @@
 package com.example.learningmanagementsystem;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class ViewStudentList extends AppCompatActivity {
 
+    ListView studentRecords;
+    ArrayList<String> myStudentRecords = new ArrayList<String>();
+    ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,19 @@ public class ViewStudentList extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        SQLiteDatabase db = openOrCreateDatabase("lmsDB", Context.MODE_PRIVATE, null);
+        studentRecords = findViewById(R.id.studentRecordList);
+
+        final Cursor cursor = db.rawQuery("select * from records", null);
+
+        int sID cursor.getColumnIndex("sID");
+        int sName cursor.getColumnIndex("sName");
+        int sSurname cursor.getColumnIndex("sSurname");
+        int sDOB cursor.getColumnIndex("sDOB");
+
+        myStudentRecords.clear();
+
+        arrayAdapter = new ArrayAdapter(this, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item, myStudentRecords);
+        studentRecords.setAdapter(arrayAdapter);
     }
 }
